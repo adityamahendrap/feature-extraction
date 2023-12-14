3561b79e466633519a9f681cfe28c76f6e20a3ea
adityamahendrap <ptadityamahendrap@gmail.com>
2023-12-14 13:54:28 +0800

    revisi

--- shape_feature_extraction/extraction.py
+++ shape_feature_extraction/extraction.py
@@ -7,15 +7,18 @@ def get_shape_features(contours):
         area = cv2.contourArea(contour)
         perimeter = cv2.arcLength(contour, True)
         
-        # Slimness
-        slimness = perimeter**2 / (4 * np.pi * area)
-        
-        # Roundness
-        roundness = (4 * area * np.pi) / (perimeter**2)
-        
-        # Rectangularity
         x, y, w, h = cv2.boundingRect(contour)
-        rectangularity = area / (w * h)
+        if area != 0:
+            # Slimness
+            slimness = perimeter**2 / (4 * np.pi * area)
+            
+            # Roundness
+            roundness = (4 * area * np.pi) / (perimeter**2)
+            
+            # Rectangularity
+            rectangularity = area / (w * h)
+        else:
+            slimness = roundness = rectangularity = 0.0
         
         # Narrow Factor
         narrow_factor = w / h if w > h else h / w
@@ -26,25 +29,36 @@ def get_shape_features(contours):
         
         # Rasio Perimeter dengan Panjang dan Lebar
         ratio_perimeter_length_width = perimeter / (2 * (w + h))
+        
+        # Chain Code
+        chain_code = get_chain_code(contour)
     
     return {
-        'area': f'{area:.2f}',
-        'perimeter': f'{perimeter:.2f}',
-        'slimness': f'{slimness:.2f}',
-        'roundness': f'{roundness:.2f}',
-        'rectangularity': f'{rectangularity:.2f}',
-        'narrow_factor': f'{narrow_factor:.2f}',
-        'ratio_perimeter_diameter': f'{ratio_perimeter_diameter:.2f}',
-        'ratio_perimeter_length_width': f'{ratio_perimeter_length_width:.2f}'
+        'area': f'{area:.3f}',
+        'perimeter': f'{perimeter:.3f}',
+        'slimness': f'{slimness:.3f}',
+        'roundness': f'{roundness:.3f}',
+        'rectangularity': f'{rectangularity:.3f}',
+        'narrow_factor': f'{narrow_factor:.3f}',
+        'ratio_perimeter_diameter': f'{ratio_perimeter_diameter:.3f}',
+        'ratio_perimeter_length_width': f'{ratio_perimeter_length_width:.3f}',
+        'chain_code': chain_code
     }
 
 def get_contours(edges):
     contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     return contours
     
-def get_hough_lines(edges):
-    lines = cv2.HoughLines(edges, 1, np.pi/180, threshold=80)
-    return lines
+def get_hough_transform(edges):
+    lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi/180, threshold=100, minLineLength=50, maxLineGap=10)
+    
+    if lines is None:
+        return edges
+    
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        cv2.line(edges, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    return edges
 
 def get_edges_with_robert(img):
     kernel_x = np.array([[1, 0], [0, -1]], dtype=np.float32)
@@ -75,10 +89,13 @@ def get_edges_with_prewitt(img):
 def get_edges_with_canny(img):
     return cv2.Canny(img, 30, 100)
       
-def get_chain_code(contours):
-    contour = contours[0]
+def get_chain_code(contour):
+    # Assuming the contour is closed
     chain_code = []
-    for point in contour:
-        x, y = point[0]
-        chain_code.append((x, y))
+    for i in range(len(contour) - 1):
+        dx = contour[i + 1][0][0] - contour[i][0][0]
+        dy = contour[i + 1][0][1] - contour[i][0][1]
+        direction = int(np.arctan2(dy, dx) * (8 / (2 * np.pi)))
+        chain_code.append(direction)
+
     return chain_code
--- shape_feature_extraction/server.py
+++ shape_feature_extraction/server.py
@@ -29,6 +29,28 @@ def save_image(edges, metode):
     cv2.imwrite(path, edges)
     return path
 
+def process_method(img, method):
+    method_mapping = {
+        'robert': ext.get_edges_with_robert,
+        'sobel': ext.get_edges_with_sobel,
+        'prewitt': ext.get_edges_with_prewitt,
+        'canny': ext.get_edges_with_canny,
+        'hough_transform': ext.get_edges_with_canny
+    }
+
+    if method in method_mapping:
+        edges = method_mapping[method](img)
+        if method is 'hough_transform':
+            edges = ext.get_hough_transform(edges)
+        result_path = save_image(edges, method)
+        contours = ext.get_contours(edges)
+        features = ext.get_shape_features(contours)
+        return {
+            'features': features,
+            'result_path': result_path,
+            'metode': 'Hough Transform' if method is 'hough_transform' else method.capitalize()
+        }
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -37,12 +59,15 @@ def home():
 def shape_feature_extraction():
     data = request.form
     initial_path = save_request_file(request)
-    
-    print(initial_path)
     img = cv2.imread(initial_path, 0)
     
     if(data['metode'] == 'hough_transform'):
-        pass
+        edges = ext.get_edges_with_canny(img)
+        edges = ext.get_hough_transform(edges)
+        result_path = save_image(edges, data['metode'])
+        contours = ext.get_contours(edges)
+        features = ext.get_shape_features(contours)
+        return render_template('result.html', initial_path=initial_path, features=features, result_path=result_path, metode='Hough Transform')
     elif(data['metode'] == 'robert'):
         edges = ext.get_edges_with_robert(img)
         result_path = save_image(edges, data['metode'])
@@ -67,6 +92,18 @@ def shape_feature_extraction():
         contours = ext.get_contours(edges)
         features = ext.get_shape_features(contours)
         return render_template('result.html', initial_path=initial_path, features=features, result_path=result_path, metode='Canny')
-    elif(data['metode'] == 'chain_code'):
-        return
+
+@app.route('/shape_feature_extraction/all', methods=['POST'])
+def shape_feature_extraction_all():
+    initial_path = save_request_file(request)
+    img = cv2.imread(initial_path, 0)
+    
+    robert_result = process_method(img, 'robert')
+    sobel_result = process_method(img, 'sobel')
+    prewitt_result = process_method(img, 'prewitt')
+    canny_result = process_method(img, 'canny')
+    hough_transform_result = process_method(img, 'hough_transform')
+    
+    return render_template('result_all.html', initial_path=initial_path, robert_result=robert_result, sobel_result=sobel_result, prewitt_result=prewitt_result, canny_result=canny_result, hough_transform_result=hough_transform_result)
+    
     
